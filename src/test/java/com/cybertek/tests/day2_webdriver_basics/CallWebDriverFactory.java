a857d5672dda6e9887f9d4315adbd888b8e8de9b
Ebubekir Bayazitoglu <ebayazitoglu@gmail.com>
2019-10-31 20:35:37 +0000

    first commit

--- src/test/java/com/cybertek/tests/day2_webdriver_basics/CallWebDriverFactory.java
+++ src/test/java/com/cybertek/tests/day2_webdriver_basics/CallWebDriverFactory.java
@@ -1,16 +1,18 @@
 package com.cybertek.tests.day2_webdriver_basics;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
 
 public class CallWebDriverFactory {
 
     public static WebDriverFactory getDriver("chrome") {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://google.com");
 
+        return driver = null;
 
-        return driver=null;
     }
 
-    {
 
 
 
